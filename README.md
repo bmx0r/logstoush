ddb9cfaad63f422b42a9898a701743077fa3a353
Michael Boutillier <michael.boutillier@atos.net>
2014-08-05 18:00:54 +0200

    forked to make a logstash poc

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-POC ES+Kibana+libnessus
+POC ES+Kibana+logstash
 =======================
 
 Quick setup of a dev env
 tested on : 
-- Vagrant version 1.2.2
-- VirtualBox 4.2.12
+- Vagrant version 1.2.2,1.6.3
+- VirtualBox 4.2.12,4.3.14
 
-git clone https://github.com/bmx0r/pytdev.git
+git clone https://github.com/bmx0r/
 
 #update the vagrant file to use an IP that is not in conflict with your network
 cd pytdev
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,43 +1,50 @@
 stage { pre: before => Stage[main] }
 
-node default{
- require  infra::base
- require  infra::database
+node my-apache{
+    package {'git':
+        ensure => present;
+    }
 
-vcsrepo { '/home/vagrant/python-nmap-lib':
- 	ensure   => present,
-  provider => git,
-	source   => 'https://github.com/savon-noir/python-nmap-lib.git',
-	owner => 'vagrant',
-	group =>  'vagrant',  
-	require => Package["git"]
-	}
-vcsrepo { '/home/vagrant/python-libnessus':
- 	ensure   => present,
-  provider => git,
-	source   => 'https://github.com/bmx0r/python-libnessus.git',
-	owner => 'vagrant',
-	group =>  'vagrant',  
-	require => Package["git"]
-	}
-
-vcsrepo { '/opt/kibana':
- 	ensure   => present,
-  	provider => git,
-	source   => 'https://github.com/elasticsearch/kibana.git',
-	owner => 'apache',
-	group =>  'apache',  
-	require => Package["git"],
-	}
-# we need the repo before configuring apache
-#Class['vcsrepo::/opt/kibana'] -> Class['apache']
-
-file { "/etc/motd":
-    owner   => root,
-    group   => root,
-    mode    => 644,
-    ensure  => present,
-	  content => "HELLO WORLD..."
-  }
+    vcsrepo { '/opt/kibana':
+        ensure   => present,
+        provider => git,
+        source   => 'https://github.com/elasticsearch/kibana.git',
+        owner    => 'apache',
+        group    =>  'apache',
+        require  => Package['git'],
+    }
+    class { 'apache':
+        default_vhost        => false,
+    }
+    apache::vhost { 'kibana.example.com':
+        port          => '80',
+        docroot       => '/opt/kibana',
+        docroot_owner => 'apache',
+        docroot_group => 'apache',
+    }
+}
+node my-es{
+# elasticSearch
+class { 'elasticsearch':
+  java_install  => true,
+  manage_repo   => true,
+  repo_version  => '1.1',
+  datadir       => '/var/lib/elasticsearch-data'
+}
+elasticsearch::instance { 'my-es-01':
+  config => {
+          'node' => {
+            'name' => $::hostname
+           },
+           'index' => {
+                       'number_of_replicas' => '0',
+                       'number_of_shards'   => '5'
+                      },
+           'cluster' => {
+                         'name' => 'ESClusterName',
+                         },
+          },
+  status => 'enabled',
+}
 
 }
