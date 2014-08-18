d510b65962adc2c7ef058d73875cdb8fe141c007
Mike Boutillier <michael.boutillier@gmail.com>
2014-08-18 08:58:22 +0200

    broken

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,11 +38,27 @@ node my-apache{
     Package['git'] -> Class['apache'] 
 }
 node my-es{
+    file{'espackage':
+      path    => '/tmp/elasticsearch-1.3.1.noarch.rpm',
+      source  => '/etc/puppet/files/elasticsearch-1.3.1.noarch.rpm',
+      ensure  => 'present',
+    }
+    file{'logstashpackage':
+      path    => '/tmp/logstash-1.4.2-1_2c0f5a1.noarch.rpm',
+      source  => '/etc/puppet/files/logstash-1.4.2-1_2c0f5a1.noarch.rpm',
+      ensure  => 'present',
+    }
+    file{'logstashcontribpackage':
+      path    => '/tmp/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm',
+      source  => '/etc/puppet/files/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm',
+      ensure  => 'present',
+    }
 # elasticSearch
 class { 'elasticsearch':
   java_install  => true,
-  manage_repo   => true,
-  repo_version  => '1.1',
+  manage_repo   => false,
+  package_url => 'file:/tmp/elasticsearch-1.3.1.noarch.rpm',
+  #repo_version  => '1.1',
   datadir       => '/var/lib/elasticsearch-data'
 }
 elasticsearch::instance { 'my-es-01':
@@ -75,5 +91,31 @@ elasticsearch::instance { 'my-es-02':
           },
   status => 'enabled',
 }
-
+# install logstash
+$config_hash = {
+        'LS_USER' => 'logstash',
+        'LS_GROUP' => 'logstash',
+        'START' => 'true',
+       }
+        
+class { 'logstash':
+  init_defaults => $config_hash,
+  package_url         => 'file:/tmp/logstash-1.4.2-1_2c0f5a1.noarch.rpm',
+  manage_repo  => false,
+  status => 'disabled',
+  #  install_contrib     => true,
+  #contrib_package_url => 'file:/tmp/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm'
+}
+  logstash::configfile { 'input_syslog':
+   content => '
+   input {
+           syslog {
+                     type => "syslog"
+                               port => "5544"
+                                       }
+                                             }
+',
+      order   => 10,
+       }
+    
 }
